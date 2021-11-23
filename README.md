a32c8aa44a761d95917dc1ea26f6c22e8043a0ca
CodrShiv <codr.shiv@gmail.com>
2021-11-23 13:11:10 +0530

    Added Icons and README

---
+++ README.md
@@ -0,0 +1,18 @@
+![Logo for XOtic](./icons/android-chrome-192x192.png)
+# XOtic
+A Web-Based TicTacToe Game implemented using Vanilla JS</p>
+
+## Getting Started
+
+You can either clone this Repository to your Local Environment or simply open XOtic online on [GitHub Pages](https://CodrShiv.GitHub.io/XOtic/index.html)
+
+
+### 
+
+1. Clone the Repository
+   ```sh
+   git clone https://github.com/CodrShiv/XOtic.git
+   ```
+2. Simply Double-Click the ```index.html``` File to open XOtic in your Favourite Browser
+
+#### Enjoy the Game!!
--- js/app.js
+++ js/app.js
@@ -1,72 +1,22 @@
 const board = document.querySelector("#board"),
-winCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-(function renderBoard() {
-  for (i = 1; i <= 9; i++) {
-    board.appendChild(document.createElement("span"));
-  }
-})();
+  winCombinations = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6],],
+  result = document.querySelector("#result");
 let choices = ["X", "O"];
+(function renderBoard() {for (i = 1; i <= 9; i++) board.appendChild(document.createElement("span"));})();
 for (const box of board.children) {
   box.onclick = (event) => {
     if (event.target.innerHTML != "") return;
     event.target.innerHTML = `<span>${choices[0]}</span>`;
-    event.target.setAttribute("class", choices[0])
-    console.log(checkWin(choices[0]))
-    if (checkWin(choices[0])) {
-      let winner = choices[0];
-      showWin(winner);
-    }
-    else if (Array.from(board.children).every((box) => box.innerHTML != "")) {
-      result.setAttribute("class", "display")
-      result.innerHTML = "Tie";
+    event.target.setAttribute("class", choices[0]);
+    if (winCombinations.some((combination) => combination.every((index) => Array.from(board.children)[index].innerHTML == `<span>${choices[0]}</span>`))) {
+      startConfetti();
+      result.setAttribute("class", "display");
+      result.querySelector("span").innerHTML = choices[0];
+      result.querySelector("span").setAttribute("class", choices[0]);
+    } else if (Array.from(board.children).every((box) => box.innerHTML != "")) {
+      result.setAttribute("class", "display");
+      result.querySelector("div").innerHTML = "<h1>It's a Tie!</h1>";
     }
     choices = choices.map((choice) => (choice === "X" ? "O" : "X"));
   };
 }
-
-function checkWin(choice) {
-    for (i=0;i<1;i++) {
-        return winCombinations.some(combination => {
-            return combination.every((index) => {
-                return Array.from(board.children)[index].innerHTML == choice
-            })
-        })
-    }
-}
-const result = document.querySelector("#result")
-function showWin(winner) {
-  startConfetti()
-  setTimeout(() => {
-    stopConfetti()
-  }, 3000);
-  result.setAttribute("class", "display");
-  result.querySelector("span").innerHTML = winner;
-  result.querySelector("span").setAttribute("class", winner)
-}
-// function showWin() {
-//   for (j = 0; j < 3; j++) {
-//     for (i = j; i < 9; i += 3) {
-//       console.log(board.children[i]);
-//     }
-//   }
-//   for (j = 0; j < 9; j += 3) {
-//     for (i = j; i < j + 3; i++) {
-//       console.log(board.children[i]);
-//     }
-//   }
-//   for (j = 0; j < 9; j += 4) {
-//     console.log(board.children[j]);
-//   }
-//   for (j = 2; j < 8; j += 2) {
-//     console.log(board.children[j]);
-//   }
-// }
